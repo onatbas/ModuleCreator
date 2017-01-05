d92cacb86a5b605c8c7f0b9d956a48643685ab3b
git@onat.me <git@onat.me>
2017-01-05 10:09:41 +0200

    Added cucumber support

---
+++ cmake/configure_gmock.cmake
@@ -0,0 +1,5 @@
+find_package(GMock REQUIRED)
+include_directories(${GMOCK_INCLUDE_DIRS})
+
+# Link against ${GMOCK_BOTH_LIBRARIES}
+
--- cmake/configure_qt5.cmake
+++ cmake/configure_qt5.cmake
@@ -1,12 +1,10 @@
 find_package(Qt5Widgets)
+find_package(Qt5Script)
 
 include_directories(${Qt5Widgets_INCLUDE_DIRS})
 
-set (QT5_LINK_TARGETS Qt5::Widgets)
+set (QT5_LINK_TARGETS Qt5::Widgets Qt5::Script)
 
-set(CMAKE_AUTOMOC ON)
-set(CMAKE_AUTOUIC ON)
-set(CMAKE_AUTORCC ON)
 
 #deploy with qt dlls.
 #link against QT5_LINK_TARGETS
