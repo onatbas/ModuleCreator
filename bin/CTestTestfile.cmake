1d62de8aa14de95e36a00b0766c47fa4eb49c6d4
git@onat.me <git@onat.me>
2017-01-14 11:16:53 +0200

    Deleted bin

--- bin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/onatbas/Desktop/ModuleEditor-master
-# Build directory: /Users/onatbas/Desktop/ModuleEditor-master/bin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
