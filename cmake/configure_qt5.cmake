21849918aa546ce978496390305e3bf1d59fad87
git@onat.me <git@onat.me>
2017-04-25 08:00:16 +0300

    Some minor adjustments

--- cmake/configure_qt5.cmake
+++ cmake/configure_qt5.cmake
@@ -5,6 +5,9 @@ include_directories(${Qt5Widgets_INCLUDE_DIRS})
 
 set (QT5_LINK_TARGETS Qt5::Widgets Qt5::Script)
 
+set(CMAKE_AUTOMOC ON)
+set(CMAKE_AUTOUIC ON)
+set(CMAKE_AUTORCC ON)
 
 #deploy with qt dlls.
 #link against QT5_LINK_TARGETS
